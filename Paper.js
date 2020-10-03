b115156c6866808d3885fd255f0431d5e0a00c47
Nisha998 <snisharithin@gmail.com>
2020-10-03 06:40:21 +0530

    Add radius

--- Paper.js
+++ Paper.js
@@ -3,10 +3,10 @@ class Paper {
       var options = {
           'isStatic':false,
           'restitution':0.3,
-          'friction':1.5,
+          'friction':0,
           'density':1.2
       }
-      this.body = Bodies.circle(x, y, 60, options);
+      this.body = Bodies.circle(x, y, (60-20/2), options);
       this.radius = 60;
       this.image = loadImage("paper.png");
            
